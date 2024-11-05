d3117cc8152c1ceea58c5e4012cbe3f1bf191c45
Aleix Amils <aleix.amils@cirvianum.cat>
2024-11-05 20:39:03 +0100

    Dist working

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
     root: "./", // Root directory for Vite
@@ -6,7 +7,25 @@ export default defineConfig({
     build: {
         outDir: "dist", // Output directory for the build
         rollupOptions: {
-            input: "./index.html", // Entry point for the build
+            input: {
+                main: path.resolve(__dirname, "index.html"),
+                completedTasks: path.resolve(__dirname, "src/pages/completedTaskList.html"),
+                favoriteTasker: path.resolve(__dirname, "src/pages/favoriteTasker.html"),
+                getStarted: path.resolve(__dirname, "src/pages/getStarted.html"),
+                giveFeedback: path.resolve(__dirname, "src/pages/giveFeedback.html"),
+                giveTips: path.resolve(__dirname, "src/pages/giveTips.html"),
+                login: path.resolve(__dirname, "src/pages/login.html"),
+                profile: path.resolve(__dirname, "src/pages/profile.html"),
+                refer: path.resolve(__dirname, "src/pages/refer.html"),
+                signup: path.resolve(__dirname, "src/pages/signup.html"),
+                task_confirm: path.resolve(__dirname, "src/pages/task_confirm.html"),
+                task_details: path.resolve(__dirname, "src/pages/task_details.html"),
+                taskChat: path.resolve(__dirname, "src/pages/taskChat.html"),
+                taskCompleted: path.resolve(__dirname, "src/pages/taskCompleted.html"),
+                taskinfo: path.resolve(__dirname, "src/pages/taskinfo.html"),
+                taskList: path.resolve(__dirname, "src/pages/taskList.html"),
+                worker_profile: path.resolve(__dirname, "src/pages/worker_profile.html")
+            },
         },
     },
     server: {
